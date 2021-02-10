ca87f293ac2b8a70d2430b5185bf5d1e9c7257f9
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-02-09 21:30:42 -0500

    feat: conexiona mongo #7

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Configuration } from './config/configuration';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen( parseInt(Configuration.environment.PORT,10) || 3000);
 }
 bootstrap();
