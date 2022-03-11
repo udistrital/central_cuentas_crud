a35061463fc0205410aa500bdfca91d8f2762734
Diego Gutierrez <diagutierrezro@unal.edu.co>
2022-03-11 12:59:22 -0500

    fix: Modificacion de entrypoint para obtener variables de db al momento de desplegar

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-#exec aws --version
+export CENTRAL_CUENTAS_MONGO_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/central_cuentas_crud/db/username --output text --query Parameter.Value)"
+export CENTRAL_CUENTAS_MONGO_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/central_cuentas_crud/db/password --output text --query Parameter.Value)"
 
 exec node dist/main
