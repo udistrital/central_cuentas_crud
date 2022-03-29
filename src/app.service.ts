0935080feacd21613f3df2b9e9d6d68cac2d50ef
Diego Gutierrez <diagutierrezro@unal.edu.co>
2022-03-29 11:27:44 -0500

    fix: ModificaciÃon de apiStatus para corregir despliegue

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,9 @@ import { environment } from './config/configuration';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return `mongodb://${environment.USER_DB}:${environment.PASS_DB}@`+
-    `${environment.HOST_DB}:${environment.PORT_DB}/${environment.NAME_DB}?authSource=${environment.AUTH_DB}`;
+  getDefault(): Object{
+    return{
+      status: "OK",
+    };
   }
 }
