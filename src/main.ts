0c02d6cce0d890320e0cea3187420a81ff37fa6c
Diego Gutierrez <diagutierrezro@unal.edu.co>
2022-06-09 20:25:42 -0500

    fix: Ajustes a dependencias udistrital/central_cuentas_cliente#167

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { environment } from './config/configuration';
 import * as fs from 'fs';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   fs.writeFileSync("./swagger.json", JSON.stringify(document, null, 2));
   SwaggerModule.setup('swagger', app, document);
-  
+  app.useGlobalPipes(new ValidationPipe());
+  await app.startAllMicroservices();
   await app.listen( parseInt(environment.PORT,10) || 3000);
 }
 bootstrap();
